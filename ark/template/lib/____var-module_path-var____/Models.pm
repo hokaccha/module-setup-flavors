85e4cbc204b4181966313d62bfee305392f91466
hokaccha <k.hokamura@gmail.com>
2010-06-25 11:10:33 +0900

    arkのモデル生成修正

--- ark/template/lib/____var-module_path-var____/Models.pm
+++ ark/template/lib/____var-module_path-var____/Models.pm
@@ -3,6 +3,8 @@ use strict;
 use warnings;
 use Ark::Models '-base';
 
+use Module::Find;
+
 register Schema => sub {
     my $self = shift;
 
@@ -17,6 +19,14 @@ for my $table (qw/Sample/) {
     register "Schema::$table" => sub {
         my $self = shift;
         $self->get('Schema')->resultset($table);
+    };
+}
+
+my @modules = Module::Find::findallmod('[% module %]::Schema::Result');
+for my $module (@modules) {
+    $module =~ s/[% module %]::Schema::Result:://;
+    register "Schema::${module}" => sub {
+        shift->get('Schema')->resultset($module);
     };
 }
 
